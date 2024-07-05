d2a723bd89995ff16fe0eb5e3f8d7c7d25e5fe1c
Adrian Barros <adriansbarros1@gmail.com>
2024-07-04 20:30:07 -0400

    display all cards in rows

--- portafolio/src/sections/Expirience.tsx
+++ portafolio/src/sections/Expirience.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import '../styles/Expirience.css';
 import Card from "../components/Card";
+import data from '../data/employment.json'
 
 
 export const Expirience = () => {
-
+    const employment = data.data
+    console.log(employment)
     return (
         <div className="title">
             <div className="expirience-section">
             </div>
+
             <div className="title-text">Expirience</div>
-            <Card />
+            <div className="card-row">
+                {employment.map((item, index) => {
+                    return (
+                        <Card />
+                    )
+                })}
+            </div>
 
         </div>
     )
