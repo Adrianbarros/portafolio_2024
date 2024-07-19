e92b4ef136d193b3f3d68764793ea6030dbef86d
Adrian Barros <adriansbarros1@gmail.com>
2024-07-18 21:45:24 -0400

    Added contact section card && pdf

--- portafolio/src/sections/ContactMe.tsx
+++ portafolio/src/sections/ContactMe.tsx
@@ -1,12 +1,24 @@
 import React, { forwardRef } from 'react'
 import '../styles/ContactMe.css'
+import { Document, Page, pdfjs } from 'react-pdf';
+import resume from '../assets/adrianBarrosRes.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
+
 
 export const ContactMe = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div className='contact-section' ref={ref} id="contact">
-            <div>
+            <div className='contact-left'>
+                <div className='contact-title'>Lets Connect!</div>
+                <div className='contact-document'>
+                    <Document file={resume}>
+                        <Page pageNumber={1} />
+                    </Document>
+                </div>
 
             </div>
+            <div className='contact-links'></div>
         </div>
     )
 })
