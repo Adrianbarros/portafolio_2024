45cb2d1e16b9d44da092580e01a226b55a75d1d4
Adrian Barros <adriansbarros1@gmail.com>
2024-07-19 21:06:22 -0400

    Added social logos

--- portafolio/src/sections/ContactMe.tsx
+++ portafolio/src/sections/ContactMe.tsx
@@ -2,6 +2,9 @@ import React, { forwardRef } from 'react'
 import '../styles/ContactMe.css'
 import { Document, Page, pdfjs } from 'react-pdf';
 import resume from '../assets/adrianBarrosRes.pdf';
+import linkedIn from '../assets/logos/linkedIn.png';
+import github from '../assets/logos/github.png';
+import gmail from '../assets/logos/gmail.png';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
 
@@ -9,6 +12,16 @@ pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.
 export const ContactMe = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div className='contact-section' ref={ref} id="contact">
+            <div className='contact-right'>
+
+                <div className='contact-subtitle'>Follow me in my socials</div>
+                <div className='Social-links'>
+                    <img className='link-logo' src={linkedIn} alt="LinkedIn logo" />
+                    <img className='link-logo' src={github} alt="LinkedIn logo" />
+                    <img className='link-logo' src={gmail} alt="LinkedIn logo" />
+                </div>
+
+            </div>
             <div className='contact-left'>
                 <div className='contact-title'>Lets Connect!</div>
                 <div className='contact-document'>
@@ -17,8 +30,9 @@ export const ContactMe = forwardRef<HTMLDivElement>((props, ref) => {
                     </Document>
                 </div>
 
+
             </div>
-            <div className='contact-links'></div>
+
         </div>
     )
 })
