308d2a709fc544c3eab3520bb6d97e6989f94018
Adrian Barros <adriansbarros1@gmail.com>
2024-08-02 21:38:16 -0400

    added functionality to pdf button

--- portafolio/src/sections/ContactMe.tsx
+++ portafolio/src/sections/ContactMe.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react'
 import { EmailForm } from '../components/emails/EmailForm'
 import '../styles/ContactMe.css'
-import resume from '../assets/adrianBarrosRes.pdf';
+import resume from '../assets/AdrianBarros_Resume.pdf';
 import linkedIn from '../assets/logos/linkedIn.png';
 import github from '../assets/logos/github.png';
 import gmail from '../assets/logos/gmail.png';
@@ -10,6 +10,14 @@ import { motion } from 'framer-motion'
 
 
 export const ContactMe = forwardRef<HTMLDivElement>((props, ref) => {
+    const handleDownload = () => {
+        const link = document.createElement("a");
+        link.href = resume;
+        link.download = "AdrianBarros_resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
     return (
         <div className='contact-section' ref={ref} id="contact">
             <div className='contact-right'>
@@ -49,20 +57,22 @@ export const ContactMe = forwardRef<HTMLDivElement>((props, ref) => {
                 <div className='other-links'>
                     <div className='contact-subtitle'>Schedule a coffee chat with me</div>
 
-                    <motion.div className='calendary-link'>
+                    <motion.button
+                        whileHover={{ scale: 1.2 }}
+                        className='calendary-link'>
                         Schedule time with me!
                         <motion.img className='small-logo' src='https://assets.calendly.com/assets/frontend/media/calendly-33a0809afc4c21162dd7.svg' />
-                    </motion.div>
+                    </motion.button>
 
                 </div>
                 <div className='other-links'>
                     <div className='contact-subtitle'>Or Download my Resume!</div>
-                    <motion.div className='resume-link'>
+                    <motion.button
+                        whileHover={{ scale: 1.2 }}
+                        onClick={() => { handleDownload() }}
+                        className='resume-link'>
                         Download PDF
-
-                    </motion.div>
-
-
+                    </motion.button>
                 </div>
 
             </div>
